4bca38eed615b9c3783e827cc490019c593a3dfb
Janik Habegger <jhabegge@uoguelph.ca>
2021-03-15 15:40:32 -0400

    Add comment to functions, make vertical stiffness more general

--- Functions/SpringForceFuction/BuckleBeam.m
+++ Functions/SpringForceFuction/BuckleBeam.m
@@ -1,4 +1,14 @@
 function F = BuckleBeam(x, P_e, a, L, q_0)
+    % The function for this code was obtained from [[1]](#1). The resultant force is the force in the vertical direction
+    % 
+    % | Parameter     | Explanation | Units
+    % | ------------- | :-------------: |:-------------: |
+    % | x             |Displacement| Meter |
+    % | P_e           |P<sub>e</sub> = EI( &pi; /L)<sup>2</sup>  - Classic Euler critical load for hinge-hinged boundary| Newtons|
+    % |a              |Horizontal distance between mass and reference (see diagram)|Meter |
+    % |L              |Full Length of the beam| Meter|
+    % |q_0            |Initial imperfection at the center of the beam| Meter|
+
     %obtained from https://www.sciencedirect.com/science/article/pii/S0022460X13000813
     gamma = a/L; %also equal to cos(theta)
     delta = 1-sqrt(1-(2*x/L)*sqrt(1-gamma^2)+(x/L)^2);
--- Functions/SpringForceFuction/F_vertSpring_y.m
+++ Functions/SpringForceFuction/F_vertSpring_y.m
@@ -1,4 +1,10 @@
 function F_y = F_vertSpring_y(x, K_v, h_0)
-    %horizontal spring y component force
-    F_y =  K_v*(x-(h_0-(2/12*.3048)));
+    % Linear approximation of springs with the resultant force acting along the axial direction (Postive stiffness). An offset can be provided to the displacment x 
+    % 
+    % | Parameter     | Explanation | Untis
+    % | ------------- | :-------------:       |:-------------: |
+    % | x             |Displacement           |Meter|
+    % | K_v           |Spring Stiffness       |Newtons/meter|
+    % |h_0            |Desired spring offset  |Meter|
+    F_y =  K_v*(x-(h_0));
 end
