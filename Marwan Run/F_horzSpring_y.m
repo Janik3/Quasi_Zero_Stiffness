108a68c0565d2a3ddb1d7f647cd7b2ca2ab38cfc
jhabegge <jhabegge@SOE-1319-49.cfs.uoguelph.ca>
2021-03-15 10:55:20 -0400

    Restructure the Documents

--- Marwan Run/F_horzSpring_y.m
+++
@@ -1,4 +0,0 @@
-function F_y = F_horzSpring_y(x, K_h, L_0, L_min, h_0)
-    %horizontal spring y component force
-    F_y =  2*K_h*(L_0 - (L_min^2+(h_0-x)^2)^(1/2))*(h_0-x)/((L_min^2+(h_0-x)^2)^(1/2));
-end
--- Marwan Run/inputFn.m
+++
@@ -1,7 +0,0 @@
-function u = inputFn(t, freq, multiplier)
-    %Input to differential equation (ground excitation)
-    
-    omega = 2*pi*freq;
-
-    u = multiplier*sin(omega*t);
-end
